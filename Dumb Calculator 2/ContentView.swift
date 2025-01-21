0304a086507f7e0cd4a4db02139b4b12f77f8435
frievoe97 <friedrich.voelkers@me.com>
2025-01-21 11:53:14 +0100

    Initial Commit

---
+++ Dumb Calculator 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dumb Calculator 2
+//
+//  Created by Friedrich Völkers on 21.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dumb Calculator 2/Dumb_Calculator_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Dumb_Calculator_2App.swift
+//  Dumb Calculator 2
+//
+//  Created by Friedrich Völkers on 21.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Dumb_Calculator_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
